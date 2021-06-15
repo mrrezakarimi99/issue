485e060ddab4acb3c9b59e695965dc5bf05b636c
reza karimi <mr.rezakarimi99@gmail.com>
2021-06-15 12:00:02 +0430

    package issue

---
+++ src/issue_management.php
@@ -0,0 +1,14 @@
+<?php
+namespace reza\issue_management;
+
+/**
+ * Laravel Issue Management Package by Reza Karimi.
+ */
+
+trait issue_management
+{
+    public function createIssue()
+    {
+
+    }
+}
---
+++ src/service_deskServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace reza\serviceDesk;
+
+use Illuminate\Support\ServiceProvider;
+
+class service_deskServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+    public function register()
+    {
+
+    }
+}
